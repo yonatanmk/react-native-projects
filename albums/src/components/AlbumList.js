a9cb02e9eecd45a32e7cdcf9f3928d770732809b
Yonatan MK <yonatanmk@gmail.com>
2017-02-19 16:06:27 -0500

    albums: added scrolling

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {View} from 'react-native';
+import {ScrollView} from 'react-native';
 import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
@@ -36,9 +36,9 @@ class AlbumList extends Component {
 
   render () {
     return (
-      <View>
+      <ScrollView>
         {this.renderAlbums()}
-      </View>
+      </ScrollView>
     );
   }
 }
