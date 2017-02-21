5de93ccb9c3c73ec5631c4fadb9b883efb8cfc57
Yonatan MK <yonatanmk@gmail.com>
2017-02-21 17:36:33 -0500

    auth: created login form

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAF9pGj6772ItA914ZApIHE3FRNh-qI4Ok',
+      authDomain: 'authentication-e398e.firebaseapp.com',
+      databaseURL: 'https://authentication-e398e.firebaseio.com',
+      storageBucket: 'authentication-e398e.appspot.com',
+      messagingSenderId: '1028841437667'
+    });
+  }
+
   render () {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text>An App!</Text>
+        <LoginForm />
       </View>
     );
   }
