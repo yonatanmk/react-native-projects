7d039f02d2434d038170fa5021c35052fb6aba79
Yonatan MK <yonatanmk@gmail.com>
2017-02-24 19:40:42 -0500

    tech_stack: lists will show desc of selected item

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+import * as actions from '../actions';
 
 class ListItem extends Component {
 
+  renderDescription() {
+    const { library, expanded } = this.props;
+    if (expanded) {
+      return (
+        <Text>{library.description}</Text>
+      );
+    }
+  }
+
   render () {
     const { titleStyle } = styles;
+    const {id, title} = this.props.library;
 
     return(
-      <CardSection style={titleStyle}>
-        <Text>
-          {this.props.library.title}
-        </Text>
-      </CardSection>
+      <TouchableWithoutFeedback
+        onPress={()=>this.props.selectLibrary(id)}
+      >
+        <View>
+          <CardSection style={titleStyle}>
+            <Text>
+              {this.props.library.title}
+            </Text>
+          </CardSection>
+          {this.renderDescription()}
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
@@ -24,4 +43,9 @@ const styles = {
   }
 };
 
-export default ListItem;
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.id;
+  return { expanded };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
