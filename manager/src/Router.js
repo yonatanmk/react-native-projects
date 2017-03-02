821fbe96fc62bebd2b6415b064ec96d069b5774d
Yonatan MK <yonatanmk@gmail.com>
2017-03-01 20:12:19 -0500

    manager: added picker

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -8,9 +8,9 @@ const RouterComponent = () => {
   return (
     <Router sceneStyle={{ paddingTop: 65 }}>
     <Scene key='auth'>
-      <Scene key='login' component={LoginForm} title='Please Login' initial />
+      <Scene key='login' component={LoginForm} title='Please Login' />
     </Scene>
-    <Scene key='main'>
+    <Scene key='main' initial>
       <Scene
         key='employeeList'
         component={EmployeeList}
