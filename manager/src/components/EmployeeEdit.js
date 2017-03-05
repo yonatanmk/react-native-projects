c79bfba0fd0f168faffe0408acfd1ba61708f25f
Yonatan MK <yonatanmk@gmail.com>
2017-03-05 17:41:54 -0500

    manager: added texting function

--- manager/src/components/EmployeeEdit.js
+++ manager/src/components/EmployeeEdit.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Communications from 'react-native-communications';
 import { employeeUpdate, employeeSave, employeeReset } from '../actions';
 import { Card, CardSection, Button } from './common';
 import EmployeeForm from './EmployeeForm';
@@ -10,6 +11,7 @@ class EmployeeEdit extends Component {
   constructor(props) {
     super(props);
     this.onButtonPress = this.onButtonPress.bind(this);
+    this.onTextPress = this.onTextPress.bind(this);
   }
 
   componentWillMount() {
@@ -27,6 +29,11 @@ class EmployeeEdit extends Component {
     this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid });
   }
 
+  onTextPress(){
+    const { name, phone, shift } = this.props;
+    Communications.text(phone, `Hello ${name}, your upcoming shift is on ${shift}`);
+  }
+
   render () {
     return (
       <Card>
@@ -34,6 +41,11 @@ class EmployeeEdit extends Component {
         <CardSection>
           <Button onPress={this.onButtonPress}>
             Save Changes
+          </Button>
+        </CardSection>
+        <CardSection>
+          <Button onPress={this.onTextPress}>
+            Text Schedule
           </Button>
         </CardSection>
       </Card>
