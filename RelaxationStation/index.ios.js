02a3e77788c88b6bc1432ce6423fae9e946e07d0
Yonatan MK <yonatanmk@gmail.com>
2017-03-29 19:08:37 -0400

    RelaxationStation user can cycle through quotes

--- RelaxationStation/index.ios.js
+++ RelaxationStation/index.ios.js
@@ -14,8 +14,32 @@ import QuoteScreen from './src/QuoteScreen';
 const { quotes } = require('./quotes.json');
 
 export default class RelaxationStation extends Component {
+  constructor(props) {
+		super(props);
+
+		this.state = {
+			quoteIndex: 3,
+		};
+
+		this._incrementQuoteIndex = this._incrementQuoteIndex.bind(this);
+	}
+
+	_incrementQuoteIndex() {
+		let newIndex;
+
+		if (this.state.quoteIndex + 1 === quotes.length) {
+			newIndex = 0;
+		} else {
+			newIndex = this.state.quoteIndex + 1;
+		}
+
+		this.setState({
+			quoteIndex: newIndex,
+		});
+	}
+
   render() {
-    const quote = quotes[2];
+    const quote = quotes[this.state.quoteIndex];
     return (
       <Navigator
         initialRoute={{ name: 'StartScreen' }}
@@ -24,7 +48,7 @@ export default class RelaxationStation extends Component {
             case 'StartScreen':
               return <StartScreen onStartHandler={()=> navigator.push({ name: 'QuoteScreen' })}/>;
             case 'QuoteScreen':
-              return <QuoteScreen text={quote.text} source={quote.source}/>;
+              return <QuoteScreen text={quote.text} source={quote.source} onNextQuotePress={this._incrementQuoteIndex}/>;
           }
         }}
       />
