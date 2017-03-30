8db58fc43c85a58d34c2ecb369fb4f33cf1199cc
Yonatan MK <yonatanmk@gmail.com>
2017-03-30 16:11:02 -0400

    BeBetter displaying service status with timestamp

---
+++ BeBriefed/src/components/App.js
@@ -0,0 +1,55 @@
+
+import React, { Component, PropTypes } from 'react';
+import {
+	View,
+	StyleSheet,
+} from 'react-native';
+// import LinearGradient from 'react-native-linear-gradient'
+//
+import StatusScreen from './StatusScreen';
+// import TabBarContainer from './TabBarContainer'
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isUp: false,
+      lastUpTime: new Date((new Date()).getTime() - 5 * 60 * 1000)
+    };
+  }
+	// _renderTab(selected, services) {
+	// 	const thisService = services.find(s => s.key === selected)
+	// 	return (
+	// 		<StatusScreen
+	// 			isUp={thisService.isUp}
+	// 			lastUpTime={thisService.lastUpTime}
+	// 		/>
+	// 	)
+	// }
+
+	render() {
+		return (
+      <View style={styles.container}>
+        <StatusScreen isUp={this.state.isUp} lastUpTime={this.state.lastUpTime}/>
+      </View>
+		);
+	}
+}
+
+App.propTypes = {
+	// selectedService: PropTypes.string.isRequired,
+	// services: PropTypes.array.isRequired,
+	// onServiceChange: PropTypes.func.isRequired,
+  // isUp: PropTypes.bool.isRequired,
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+});
+
+export default App;
---
+++ BeBriefed/src/components/StatusScreen.js
@@ -0,0 +1,58 @@
+import React, { Component, PropTypes } from 'react';
+import {
+	View,
+	Text,
+	StyleSheet,
+} from 'react-native';
+import moment from 'moment';
+
+import StatusIndicator from './StatusIndicator';
+
+
+class StatusScreen extends Component {
+	render() {
+		let lastUpComponent;
+		if (!this.props.isUp) {
+			const relativeTime = moment().to(this.props.lastUpTime);
+			lastUpComponent = <Text style={styles.lastUpText}>Last up: {relativeTime}</Text>;
+		}
+
+		return (
+			<View style={styles.container}>
+				<StatusIndicator isUp={this.props.isUp} />
+				<Text style={styles.statusText}>Service {this.props.isUp ? 'Up' : 'Down!'}</Text>
+				{lastUpComponent}
+			</View>
+		);
+	}
+}
+
+StatusScreen.propTypes = {
+	isUp: PropTypes.bool.isRequired,
+	lastUpTime: PropTypes.instanceOf(Date),
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	statusText: {
+		fontSize: 40,
+		fontWeight: 'bold',
+		marginTop: 60,
+		textAlign: 'center',
+		color: '#E6E8EF',
+		backgroundColor: 'rgba(0,0,0,0)',
+	},
+	lastUpText: {
+		fontSize: 20,
+		marginTop: 20,
+		textAlign: 'center',
+		color: '#b1b3b6',
+		backgroundColor: 'rgba(0,0,0,0)',
+	},
+});
+
+export default StatusScreen;
