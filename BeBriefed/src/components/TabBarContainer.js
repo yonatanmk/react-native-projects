cde19a64829837c8ecce393883061d1ef133f78b
Yonatan MK <yonatanmk@gmail.com>
2017-03-30 16:45:25 -0400

    BeBetter added tab icons

--- BeBriefed/src/components/TabBarContainer.js
+++ BeBriefed/src/components/TabBarContainer.js
@@ -5,21 +5,28 @@ import {
 	StyleSheet,
 } from 'react-native';
 import Tabs from 'react-native-tabs';
+import TabBarItem from './TabBarItem';
 
 const TabBarContainer = (props) => (
-  <Tabs>
-    <Text>Web</Text>
-    <Text>Database</Text>
-    <Text>Mail</Text>
+  <Tabs style={styles.tabContainer}>
+    <TabBarItem name="web" label="Web Server" icon="server" />
+    <TabBarItem name="db" label="DB Server" icon="database" />
+    <TabBarItem name="mail" label="Mail Server" icon="envelope-o" />
   </Tabs>
 );
 
 // TabBarContainer.propTypes = {
-// 	isUp: PropTypes.bool.isRequired,
+// 	onTabChange: PropTypes.func.isRequired,
+// 	selectedService: PropTypes.string.isRequired,
 // };
 
 const styles = StyleSheet.create({
-
+	tabContainer: {
+		backgroundColor: '#343434',
+		borderTopWidth: 1,
+		borderTopColor: '#262626',
+		height: 96,
+	},
 });
 
 export default TabBarContainer;
