fbf31e749fc00799dfe607192ff78a86d1de348e
Yonatan MK <yonatanmk@gmail.com>
2017-03-30 18:06:50 -0400

    ChatNow added button main page

--- ChatNow/index.ios.js
+++ ChatNow/index.ios.js
@@ -1,53 +1,24 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+	AppRegistry,
 } from 'react-native';
+// import { Provider } from 'react-redux';
+// import { createStore, applyMiddleware } from 'redux';
+// import thunk from 'redux-thunk';
 
-export default class ChatNow extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+import MainScreen from './src/components/MainScreen';
+
+// import AppContainer from './containers/AppContainer';
+// import chatNowReducers from './reducers';
+
+// let store = createStore(chatNowReducers, applyMiddleware(thunk));
+
+class ChatNow extends Component {
+	render() {
+		return (
+      <MainScreen />
+		);
+	}
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('ChatNow', () => ChatNow);
