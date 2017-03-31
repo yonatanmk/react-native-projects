26ffc1fecd55b605793a45ab1637bb3a6f6d08c9
Yonatan MK <yonatanmk@gmail.com>
2017-03-31 14:53:35 -0400

    ChatNow added store to index.ios

--- ChatNow/index.ios.js
+++ ChatNow/index.ios.js
@@ -2,21 +2,23 @@ import React, { Component } from 'react';
 import {
 	AppRegistry,
 } from 'react-native';
-// import { Provider } from 'react-redux';
-// import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 // import thunk from 'redux-thunk';
 
 import App from './src/components/App';
 
 // import AppContainer from './containers/AppContainer';
-// import chatNowReducers from './reducers';
+import chatNowReducers from './reducers';
 
-// let store = createStore(chatNowReducers, applyMiddleware(thunk));
+let store = createStore(chatNowReducers);
 
 class ChatNow extends Component {
 	render() {
 		return (
-      <App />
+			<Provider store={store}>
+				<App />
+			</Provider>
 		);
 	}
 }
