991dc3e0568dc0fc5fde56b353eab9f2d63691e7
Yonatan MK <yonatanmk@gmail.com>
2017-03-31 14:35:45 -0400

    ChatNow added actions/reducers

---
+++ ChatNow/src/actions/index.js
@@ -0,0 +1,59 @@
+export function updateName(name) {
+	return {
+		type: 'UPDATE_NAME',
+		name,
+	};
+}
+
+export function updateAccountNumber(number) {
+	return {
+		type: 'UPDATE_ACCOUNT_NUMBER',
+		number,
+	};
+}
+
+export function rehydrateNameAndAccountNum(name, accountNumber) {
+	return {
+		type: 'REHYDRATE_CUSTOMER_DETAILS',
+		name,
+		number: accountNumber,
+	};
+}
+
+export function updateComposeMessage(message) {
+	return {
+		type: 'UPDATE_COMPOSE_MESSAGE',
+		message,
+	};
+}
+
+export function sendMessage(timestamp) {
+	return (dispatch, getState) => {
+		const message = {
+			message: getState().composingMessage,
+			timestamp,
+			customerName: getState().name,
+			accountNumber: getState().accountNumber,
+		};
+
+		dispatch({
+			type: 'SEND_MESSAGE',
+			...message,
+		});
+
+		fetch('http://localhost:8080/messages', {
+			method: 'POST',
+			headers: new Headers({
+				'Content-Type': 'application/json',
+			}),
+			body: JSON.stringify(message),
+		});
+	};
+}
+
+export function receivedMessage(message) {
+	return {
+		type: 'RECEIVED_MESSAGE',
+		message,
+	};
+}
---
+++ ChatNow/src/reducers/index.js
@@ -0,0 +1,60 @@
+import { combineReducers } from 'redux';
+
+function name(state = '', action) {
+	switch (action.type) {
+	case 'UPDATE_NAME':
+	case 'REHYDRATE_CUSTOMER_DETAILS':
+		console.log('Setting name to :', action.name);
+		return action.name;
+	default:
+		console.log('Returning default state:', state);
+		return state;
+	}
+}
+
+function accountNumber(state = '', action) {
+	switch (action.type) {
+	case 'UPDATE_ACCOUNT_NUMBER':
+	case 'REHYDRATE_CUSTOMER_DETAILS':
+		return action.number;
+	default:
+		return state;
+	}
+}
+
+function composingMessage(state = '', action) {
+	switch (action.type) {
+	case 'UPDATE_COMPOSE_MESSAGE':
+		return action.message;
+	case 'SEND_MESSAGE':
+		return '';
+	default:
+		return state;
+	}
+}
+
+function messages(state = [], action) {
+	switch (action.type) {
+	case 'SEND_MESSAGE':
+		return [
+			...state,
+			{ message: action.message, timestamp: action.timestamp, isOwnMessage: true },
+		];
+	case 'RECEIVED_MESSAGE':
+		return [
+			...state,
+			action.message,
+		];
+	default:
+		return state;
+	}
+}
+
+const chatNowReducers = combineReducers({
+	name,
+	accountNumber,
+	composingMessage,
+	messages,
+});
+
+export default chatNowReducers;
