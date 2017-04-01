5f6847f53be9b566619f0d9c3ca42d89a2c904ab
Yonatan MK <yonatanmk@gmail.com>
2017-04-01 15:55:47 -0400

    codenamesGenerator basic app

---
+++ codenamesGenerator/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+class App extends Component {
+  render () {
+
+    return (
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
+
+export default App;
