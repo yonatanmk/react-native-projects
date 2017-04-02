9bca5658d4e53752b364073d9df1d3c615aacffc
Yonatan MK <yonatanmk@gmail.com>
2017-04-02 18:07:38 -0400

    codenamesGenerator created responsize grid in flex

--- codenamesGenerator/src/components/Board.js
+++ codenamesGenerator/src/components/Board.js
@@ -5,66 +5,56 @@ class Board extends Component {
   render () {
     return (
       <View style={styles.container}>
-          <View style={styles.box2}></View>
-          <View style={styles.box2}></View>
-          <View style={styles.box2}></View>
-          <View style={styles.box2}></View>
-          <View style={styles.box2}></View>
+        <View style={styles.rowContainer}>
+          <View style={styles.box}></View>
+          <View style={styles.box}></View>
+          <View style={styles.box}></View>
+          <View style={styles.box}></View>
+          <View style={styles.box}></View>
+        </View>
+        <View style={styles.rowContainer}>
+          <View style={styles.box}></View>
+          <View style={styles.box}></View>
+          <View style={styles.box}></View>
+          <View style={styles.box}></View>
+          <View style={styles.box}></View>
+        </View>
+        <View style={styles.rowContainer}>
+          <View style={styles.box}></View>
+          <View style={styles.box}></View>
+          <View style={styles.box}></View>
+          <View style={styles.box}></View>
+          <View style={styles.box}></View>
+        </View>
+        <View style={styles.rowContainer}>
+          <View style={styles.box}></View>
+          <View style={styles.box}></View>
+          <View style={styles.box}></View>
+          <View style={styles.box}></View>
+          <View style={styles.box}></View>
+        </View>
+        <View style={styles.rowContainer}>
+          <View style={styles.box}></View>
+          <View style={styles.box}></View>
+          <View style={styles.box}></View>
+          <View style={styles.box}></View>
+          <View style={styles.box}></View>
+        </View>
       </View>
     );
-
-    // return (
-    //   <View style={styles.container}>
-    //     <View style={styles.rowContainer}>
-    //       <View style={styles.box}></View>
-    //       <View style={styles.box}></View>
-    //       <View style={styles.box}></View>
-    //       <View style={styles.box}></View>
-    //       <View style={styles.box}></View>
-    //     </View>
-    //     <View style={styles.rowContainer}>
-    //       <View style={styles.box}></View>
-    //       <View style={styles.box}></View>
-    //       <View style={styles.box}></View>
-    //       <View style={styles.box}></View>
-    //       <View style={styles.box}></View>
-    //     </View>
-    //     <View style={styles.rowContainer}>
-    //       <View style={styles.box}></View>
-    //       <View style={styles.box}></View>
-    //       <View style={styles.box}></View>
-    //       <View style={styles.box}></View>
-    //       <View style={styles.box}></View>
-    //     </View>
-    //     <View style={styles.rowContainer}>
-    //       <View style={styles.box}></View>
-    //       <View style={styles.box}></View>
-    //       <View style={styles.box}></View>
-    //       <View style={styles.box}></View>
-    //       <View style={styles.box}></View>
-    //     </View>
-    //     <View style={styles.rowContainer}>
-    //       <View style={styles.box}></View>
-    //       <View style={styles.box}></View>
-    //       <View style={styles.box}></View>
-    //       <View style={styles.box}></View>
-    //       <View style={styles.box}></View>
-    //     </View>
-    //   </View>
-    // );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    flexDirection: 'column',
-    // alignItems: 'flex-start',
+    // borderWidth: 2,
+    // borderColor: 'black',
+    marginLeft: 30,
+    marginRight: 30,
+    marginTop: 40,
   },
   rowContainer: {
-    flex: 1,
     flexDirection: 'row',
-    // alignItems: 'flex-start',
   },
   box: {
     flex: 1,
@@ -72,14 +62,6 @@ const styles = StyleSheet.create({
     backgroundColor: 'red',
     borderWidth: 2,
     borderColor: 'black',
-  },
-  box2: {
-    flex: 1,
-    height: 60,
-    width: 60,
-    backgroundColor: 'red',
-    // borderWidth: 2,
-    // borderColor: 'black',
   }
 });
 
