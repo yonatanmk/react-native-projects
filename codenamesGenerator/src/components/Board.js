633bff0122929e47b29774ba78543b2e428d2f66
Yonatan MK <yonatanmk@gmail.com>
2017-04-02 18:27:44 -0400

    codenamesGenerator generating board from arr

--- codenamesGenerator/src/components/Board.js
+++ codenamesGenerator/src/components/Board.js
@@ -1,45 +1,43 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+import {shuffle} from '../../utilities';
+
 class Board extends Component {
   render () {
+
+    let rows = [
+      [ 1, 4, 0, 0 ],
+      [ 2, 1, 0, 2 ],
+      [ 2, 0, 1, 2 ],
+      [ 2, 1, 0, 2 ],
+      [ 2, 2, 0, 1 ]
+    ];
+
+    rows = rows.map((row)=>{
+      let boxes = [];
+      for (let i = row[0]; i > 0 ; i--) {
+        boxes.push(<View style={[styles.box, styles.red]}></View>);
+      }
+      for (let i = row[1]; i > 0 ; i--) {
+        boxes.push(<View style={[styles.box, styles.blue]}></View>);
+      }
+      for (let i = row[2]; i > 0 ; i--) {
+        boxes.push(<View style={[styles.box, styles.black]}></View>);
+      }
+      for (let i = row[3]; i > 0 ; i--) {
+        boxes.push(<View style={styles.box}></View>);
+      }
+      return (
+        <View style={styles.rowContainer}>
+          {shuffle(boxes)}
+        </View>
+      );
+    });
+
     return (
       <View style={styles.container}>
-        <View style={styles.rowContainer}>
-          <View style={styles.box}></View>
-          <View style={styles.box}></View>
-          <View style={styles.box}></View>
-          <View style={styles.box}></View>
-          <View style={styles.box}></View>
-        </View>
-        <View style={styles.rowContainer}>
-          <View style={styles.box}></View>
-          <View style={styles.box}></View>
-          <View style={styles.box}></View>
-          <View style={styles.box}></View>
-          <View style={styles.box}></View>
-        </View>
-        <View style={styles.rowContainer}>
-          <View style={styles.box}></View>
-          <View style={styles.box}></View>
-          <View style={styles.box}></View>
-          <View style={styles.box}></View>
-          <View style={styles.box}></View>
-        </View>
-        <View style={styles.rowContainer}>
-          <View style={styles.box}></View>
-          <View style={styles.box}></View>
-          <View style={styles.box}></View>
-          <View style={styles.box}></View>
-          <View style={styles.box}></View>
-        </View>
-        <View style={styles.rowContainer}>
-          <View style={styles.box}></View>
-          <View style={styles.box}></View>
-          <View style={styles.box}></View>
-          <View style={styles.box}></View>
-          <View style={styles.box}></View>
-        </View>
+        {rows}
       </View>
     );
   }
@@ -59,10 +57,19 @@ const styles = StyleSheet.create({
   box: {
     flex: 1,
     height: 60,
-    backgroundColor: 'red',
+    backgroundColor: '#DFCC9D',
     borderWidth: 2,
     borderColor: 'black',
-  }
+  },
+  red: {
+    backgroundColor: '#EB1E2F',
+  },
+  blue: {
+    backgroundColor: '#077EAB',
+  },
+  black: {
+    backgroundColor: '#373737',
+  },
 });
 
 export default Board;
