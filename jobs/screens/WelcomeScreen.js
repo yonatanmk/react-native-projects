959f006e78734149603bcecc6383878e5d74ea8b
Yonatan MK <yonatanmk@gmail.com>
2017-04-16 19:10:51 -0400

    jobs can skip wlecome screen

--- jobs/screens/WelcomeScreen.js
+++ jobs/screens/WelcomeScreen.js
@@ -1,5 +1,7 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, AsyncStorage } from 'react-native';
+import { AppLoading } from 'expo';
 import Slides from '../components/Slides';
 
 const SLIDE_DATA = [
@@ -9,11 +11,28 @@ const SLIDE_DATA = [
 ];
 
 class WelcomeScreen extends Component {
+  state = { token: null }
+
+  async componentWillMount() {
+    let token = await AsyncStorage.getItem('fb_token');
+
+    if (token) {
+      this.setState({ token });
+      this.props.navigation.navigate('map');
+    } else {
+      this.setState({ token: false });
+    }
+  }
+
   onSlidesComplete = () => { //arrow func means you dont have to bind this when passing to child prop
     this.props.navigation.navigate('auth') //if screen component is rendered by navigator from react-navigation then screen component will have navigation prop
   }
 
   render() {
+    if (_.isNull(this.state.token)) {
+      return <AppLoading />
+    }
+
     return (
       <Slides
         data={SLIDE_DATA}
